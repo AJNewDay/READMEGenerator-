2714456641227d22c280d20ace7c0ecebd400762
Aaron Jordan <aabjordan88@gmail.com>
2023-01-22 20:10:03 -0500

    next commit

--- index.js
+++ index.js
@@ -1,33 +1,43 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
-const generateMarkdown = require("./utils/generateMarkdown");
+const generateMarkdown = require("./generateMarkdown");
 
 // TODO: Create an array of questions for user input
 // This should include what the app is for, how to use the app, how to install it, how to report issues, and how to make contributions
-const questions = [
-  "What is your app for?",
-  "How can this app be used?",
-  "How is this app installed?",
-  "How can issues be reported?",
-  "How can contributions be made?",
-];
+// const test = [
+//   "What is your app for?",
+//   "How can this app be used?",
+//   "How is this app installed?",
+//   "How can issues be reported?",
+//   "How can contributions be made?",
+// ];
 
-const test = [
+const questions = [
   {
     type: "input",
-    name: "title",
+    name: "first",
     message: "What is the title of the Project?",
   },
   {
     type: "input",
     name: "second",
-    message: "What is the description of the Project?",
+    message: "What is the app used for?",
   },
   {
     type: "input",
     name: "third",
-    message: "What is the something of the Project?",
+    message: "How is this app installed?",
+  },
+  {
+    type: "input",
+    name: "fourth",
+    message: "How can issues be reported?",
+  },
+  {
+    type: "input",
+    name: "fifth",
+    message: "How can contributions be made?",
   },
 ];
 
@@ -40,7 +50,7 @@ const test = [
 // );
 // });
 
-function writeToFile(README, data) {
+function writeToFile(README, answers) {
   // we write our code to WRITE NEW DATA to the FILE SYSTEM (fs)
 }
 
@@ -49,12 +59,14 @@ function init() {
   // here we start our logic/code
 
   inquirer
-    .prompt(test)
+    .prompt(questions)
     .then((answers) => {
       console.log(answers);
       console.log(answers.first);
       console.log(answers.second);
       console.log(answers.third);
+      console.log(answers.fourth);
+      console.log(answers.fifth);
 
       // IF we need data from the prompt method continue our code inside of this functions scope
       // we can declare global variable and then update the value
